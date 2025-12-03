5c23694014cd08164caf64facda12a618ce3c352
Md. Rajibul Islam Shuvo <rajibulislam3777@gmail.com>
2025-12-03 16:00:07 +0600

    ready4

--- mango_bar/settings.py
+++ mango_bar/settings.py
@@ -198,7 +198,7 @@ cloudinary.config(
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-STATICFILES_DIR = [BASE_DIR/'static']
+STATICFILES_DIRS = [BASE_DIR/'static']
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"

df7d93df97e72f49daa25d446dbfe0968806d776
Md. Rajibul Islam Shuvo <rajibulislam3777@gmail.com>
2026-03-06 20:04:57 +0600

    Fix IsAdminSeller permission to allow GET requests

--- products/permissions.py
+++ products/permissions.py
@@ -1,11 +1,9 @@
-from rest_framework.permissions import BasePermission
-
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsAdminSeller(BasePermission):
     def has_permission(self, request, view):
-        if request.method in ("POST", "PUT", "PATCH", "DELETE"):   
+        if request.method in SAFE_METHODS:   
             return True
-        
         
         return (
             request.user.is_authenticated and 
